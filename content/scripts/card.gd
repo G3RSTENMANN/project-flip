2aeb45d0af8f3a4a0e1cc702c41af144652ea3f5
Luca Pabis <luca@pabis.de>
2024-12-22 00:48:40 +0100

    initial code base

---
+++ content/scripts/card.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+signal hovered
+signal hovered_off
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_parent().connect_card_signals(self)
+
+
+func _on_area_2d_mouse_entered() -> void:
+	hovered.emit(self)
+
+
+func _on_area_2d_mouse_exited() -> void:
+	hovered_off.emit(self)
